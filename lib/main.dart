32f5c909bc731d12e02a7c14b6b9dffa1dcd31c5
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-04 21:56:43 +0600

    add textField

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -29,7 +30,7 @@ class _TipCalculatorState extends State<TipCalculator> {
         centerTitle: true,
       ),
       body: Container(
-        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*.1),
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * .1),
         color: Colors.white10,
         alignment: Alignment.center,
         child: ListView(
@@ -37,25 +38,57 @@ class _TipCalculatorState extends State<TipCalculator> {
           padding: EdgeInsets.all(20.0),
           children: <Widget>[
             Container(
-              height: 150,
-              width: 150,
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.teal.withOpacity(.3),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Total Per Person'),
+                      Text('\$168'),
+                    ],
+                  ),
+                ),),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              padding: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                color: Colors.teal,
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.blueGrey.shade100,
+                  style: BorderStyle.solid,
+                ),
+                borderRadius: BorderRadius.circular(16.0),
 
               ),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-
                 children: <Widget>[
-                  Text('Total Per Person'),
-                  Text('\$168'),
+                  TextField(
+                    keyboardType:TextInputType.numberWithOptions(decimal: true) ,
+                    style: TextStyle(color: Colors.teal),
+                    decoration: InputDecoration(
+//                      prefixText: ('Bill Amount'),
+                      prefixIcon: Icon(Icons.attach_money),
+                    ),
+                    onChanged: (String value){
+                      try{
+                        __billAmount = double.parse(value);
+                      }
+                      catch(exception){
+                        __billAmount = 0.0;
+                      }
+                    },
+
+                  ),
                 ],
-              )
+              ),
             ),
           ],
         ),
-
       ),
     );
   }
